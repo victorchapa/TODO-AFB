89257b5dfb6b3f2ad16a3cb142c5aa227fb07c16
Víc <victor_260690@hotmail.com>
2023-06-06 01:11:31 -0600

    Added based component and the FireBase config.

---
+++ TODOApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TODOApp/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styles: [
+  ]
+})
+export class TodoListComponent {
+
+}
